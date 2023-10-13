01d5e815d48810a0a8428b89d7be3197cb8c2ec8
Odair <odairodriguez@yahoo.com.br>
2023-10-13 11:10:12 -0300

    scaffolding

---
+++ src/components/button/index.tsx
@@ -0,0 +1,34 @@
+import styles from './styles.module.scss';
+
+export default function Exemplos(){
+    //Os botões estão padronizados em: src/scss/abstracts/_mixin.scss 
+    //aqui apenas exemplifico o seu uso. 
+    return(
+        <div className={styles.container}>
+            <button className={styles.btnTeste1}>Text Button</button>
+            <button className={styles.btnTeste2}>Text Button</button>
+            <button className={styles.btnTeste3}>Text Button</button>
+            <button className={styles.btnTeste4}>Text Button</button>
+            <button className={styles.btnTeste5}>Text Button</button>
+            <button className={styles.btnTeste6}>Text Button</button>
+            <button className={styles.btnTeste7}>Text Button</button>
+            <button className={styles.btnTeste8}>Text Button</button>
+            <button className={styles.btnTeste9}>Text Button</button>
+            <button className={styles.btnTeste10}>Text Button</button>
+            <button className={styles.btnTeste11}>Text Button</button>
+            <button className={styles.btnTeste12}>Text Button</button>
+            <button className={styles.btnTeste13}>Text Button</button>
+            <button className={styles.btnTeste14}>Text Button</button>
+            <button className={styles.btnTeste15}>Text Button</button>
+            <button className={styles.btnTeste16}>Text Button</button>
+            <button className={styles.btnTeste17}>Text Button</button>
+            <button className={styles.btnTeste18}>Text Button</button>
+            <button className={styles.btnTeste19}>Text Button</button>
+            <button className={styles.btnTeste20}>Text Button</button>
+            <button className={styles.btnTeste21}>Text Button</button>
+            <button className={styles.btnTeste22}>Text Button</button>
+            <button className={styles.btnTeste23}>Text Button</button>
+            <button className={styles.btnTeste24}>Text Button</button>
+        </div>
+    )
+}
---
+++ src/components/modal/index.tsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.scss';
+import { Lexend } from '@next/font/google';
+
+const lexend = Lexend({
+    subsets: ['latin'],
+    weight: ['100','200','300','400','500','600','700','800','900']
+})
+
+export default function Modal(){
+    return(
+        <div className={styles.container}>
+            <div className={styles.modal}>
+                <div className={styles.description}>
+                    <p className={lexend.className}>Imagem do veículo</p>
+                    <div className={styles.btnClose}>x</div>
+                </div>
+                <div className={styles.divImg}></div>
+            </div>
+        </div>
+    )
+}
