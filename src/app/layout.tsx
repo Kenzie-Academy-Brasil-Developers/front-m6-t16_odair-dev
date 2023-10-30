de6c231f602715566ba519edccb9d97eab3c7d56
Odair <odairodriguez@yahoo.com.br>
2023-10-30 15:53:36 -0300

    adjustment dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,41 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '@/scss/main.scss';
-import { GlobalProvider } from '@/providers/GlobalContext';
-import Header from '@/components/header';
-import Footer from '@/components/footer';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/scss/main.scss";
+import { GlobalProvider } from "@/providers/GlobalContext";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import ModalUpdateUser from "@/components/modal/modalUser/update";
+import ModalUpdateAddress from "@/components/modal/modalAddress/update";
+import ModalToastRegister from "@/components/modal/modalToast/register";
+import ModalToastDelete from "@/components/modal/modalToast/delete";
+import ModalToastNewAnnouncement from "@/components/modal/modalToast/announcement";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Kenzie Veículos',
-  description: 'Site de comercialização de veículos',
-}
+  title: "Kenzie Veículos",
+  description: "Site de comercialização de veículos",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <GlobalProvider>
-          <Header/>
-            {children}
-          <Footer/>
+          <ModalUpdateUser />
+          <ModalUpdateAddress />
+          <ModalToastRegister />
+          <ModalToastDelete />
+          <ModalToastNewAnnouncement />
+          <Header />
+          {children}
+          <Footer />
         </GlobalProvider>
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,10 @@
 import Banner from "@/components/banner";
 import Card, { IAnnouncements } from "@/components/card";
 import Container from "@/components/container";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
-import Modal from "@/components/modal";
 import { GlobalContext } from "@/providers/GlobalContext";
-import { api } from "@/services/api";
 import { useContext, useEffect } from 'react';
 
 export default function Home() {
-  // const { data } = await api.get("/announcements");
   const { getAnnouncements, announcements, setAadvertiser } = useContext(GlobalContext);
 
   useEffect(()=>{
